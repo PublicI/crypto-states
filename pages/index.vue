102bac93dc68e48b7c585dc37386f7a22f984f70
Chris Zubak-Skees <chriszs@gmail.com>
2018-10-24 19:03:00 -0400

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <statebin :rows="rows" />
+</template>
+
+<script>
+import rows from '~/assets/State Ethics Offices - State List.csv'
+import Statebin from '~/components/Statebin.vue';
+
+export default {
+    data() {
+        return {
+            rows: rows.map(row => {
+                return {
+                    state: row.State,
+                    regulations: row.Regulations ? row.Regulations : 'No data'
+                };
+            })
+        };
+    },
+    components: {
+        Statebin
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
